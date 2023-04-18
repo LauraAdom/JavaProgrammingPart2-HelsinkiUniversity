f010372a91ec35973de8864665dd6be51c7840d4
LauraAdom <laura.adomaityt@gmail.com>
2023-04-18 23:23:03 +0200

    Inheritence, part 9 exercise 3 fixed

--- src/main/java/Part9/Inheritance/Exercise3/ProductWarehouseWithHistory.java
+++ src/main/java/Part9/Inheritance/Exercise3/ProductWarehouseWithHistory.java
@@ -1,14 +1,12 @@
 package Part9.Inheritance.Exercise3;
 
-import java.util.ArrayList;
-
 public class ProductWarehouseWithHistory extends ProductWarehouse {
 
-    private ArrayList<Double> history;
+    private ChangeHistory history;
 
     public ProductWarehouseWithHistory(String productName, double capacity, double initialBalance) {
         super(productName, capacity);
-        this.history = new ArrayList<>();
+        this.history = new ChangeHistory();
         this.history.add(initialBalance);
         super.setBalance(initialBalance);
     }
@@ -16,34 +14,33 @@ public class ProductWarehouseWithHistory extends ProductWarehouse {
     @Override
     public void addToWarehouse(double amount) {
         super.addToWarehouse(amount);
-        this.history.add(this.history.get(this.history.size() - 1) + amount);
+        this.history.add(this.history.getHistory().get(this.history.getHistory().size() - 1) + amount);
     }
 
     @Override
     public double takeFromWarehouse(double amount) {
-        this.history.add(this.history.get(this.history.size() - 1) - amount);
+        this.history.add(this.history.getHistory().get(this.history.getHistory().size() - 1) - amount);
         return super.takeFromWarehouse(amount);
     }
 
     public String history() {
         String history = "[";
-        for (int i = 0; i < this.history.size(); i++) {
-            if (i != this.history.size() - 1) {
-                history += String.valueOf(this.history.get(i)) + ", ";
+        for (int i = 0; i < this.history.getHistory().size(); i++) {
+            if (i != this.history.getHistory().size() - 1) {
+                history += String.valueOf(this.history.getHistory().get(i)) + ", ";
             } else {
-                history += String.valueOf(this.history.get(i)) + "]";
+                history += String.valueOf(this.history.getHistory().get(i)) + "]";
             }
         }
         return history;
     }
 
     public void printAnalysis() {
-        ChangeHistory ch = new ChangeHistory();
         System.out.println(
                 "Product: " + this.getName() +
                         "\nHistory: " + history() +
-                        "\nLargest amount of product: " + ch.maxValue() +
-                        "\nSmallest amount of product: " + ch.minValue() +
-                        "\nAverage: " + ch.average());
+                        "\nLargest amount of product: " + this.history.maxValue() +
+                        "\nSmallest amount of product: " + this.history.minValue() +
+                        "\nAverage: " + this.history.average());
     }
 }
