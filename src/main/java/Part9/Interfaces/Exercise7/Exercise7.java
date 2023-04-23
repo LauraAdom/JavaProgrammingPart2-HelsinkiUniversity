1b14034055a2ca15b48cbe78ea942cf47921d920
LauraAdom <laura.adomaityt@gmail.com>
2023-04-23 17:19:56 +0200

    Part 9, Interfaces, exercise 7

---
+++ src/main/java/Part9/Interfaces/Exercise7/Exercise7.java
@@ -0,0 +1,10 @@
+package Part9.Interfaces.Exercise7;
+
+import java.util.Map;
+
+public class Exercise7 {
+
+    public static int returnSize(Mgap<String, String> names) {
+        return names.size();
+    }
+}
