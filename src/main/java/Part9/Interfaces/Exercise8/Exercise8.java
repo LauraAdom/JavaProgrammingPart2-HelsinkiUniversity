cf9885ce21a1266c7f4e5887b44fa60d61792590
LauraAdom <laura.adomaityt@gmail.com>
2023-04-24 19:08:35 +0200

    Part 9, Interfaces, exercise 8

---
+++ src/main/java/Part9/Interfaces/Exercise8/Exercise8.java
@@ -0,0 +1,10 @@
+package Part9.Interfaces.Exercise8;
+
+import java.util.Map;
+
+public class Exercise8 {
+
+    public static int returnSize(Map<String,String> names) {
+        return names.size();
+    }
+}
