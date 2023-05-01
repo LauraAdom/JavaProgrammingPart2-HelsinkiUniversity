386157cad7a17ede0e4fc8c70815a521249e4710
LauraAdom <laura.adomaityt@gmail.com>
2023-05-01 22:34:13 +0200

    Part 9, Interfaces, exercise 12

---
+++ src/main/java/Part9/Interfaces/Exercise12/Animal.java
@@ -0,0 +1,22 @@
+package Part9.Interfaces.Exercise12;
+
+public abstract class Animal {
+
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public void eat() {
+        System.out.println(getName() + " eats");
+    }
+
+    public void sleep() {
+        System.out.println(getName() + " sleeps");
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
---
+++ src/main/java/Part9/Interfaces/Exercise12/NoiseCapable.java
@@ -0,0 +1,6 @@
+package Part9.Interfaces.Exercise12;
+
+public interface NoiseCapable {
+
+    public void makeNoise();
+}
