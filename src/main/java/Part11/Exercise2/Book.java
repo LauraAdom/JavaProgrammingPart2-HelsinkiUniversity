5bd82015a68feec529926e3153821be75e7430b9
LauraAdom <laura.adomaityt@gmail.com>
2023-05-25 00:00:21 +0200

    Part 11, exercise 2

---
+++ src/main/java/Part11/Exercise2/Book.java
@@ -0,0 +1,8 @@
+package Part11.Exercise2;
+
+public class Book {
+
+    private String name;
+    private String author;
+    private int pageCount;
+}
---
+++ src/main/java/Part11/Exercise2/Plane.java
@@ -0,0 +1,8 @@
+package Part11.Exercise2;
+
+public class Plane {
+
+    private String ID;
+    private String model;
+    private int yearOfIntroduction;
+}
